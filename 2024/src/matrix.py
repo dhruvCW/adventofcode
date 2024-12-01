c479bcd4cad73a0874057f924c5c44e8dc62ee23
Dhruv Paranjape <d.paranjape@catawiki.nl>
2024-12-01 14:24:06 +0100

    refactor matrix class

--- 2024/src/matrix.py
+++ 2024/src/matrix.py
@@ -1,35 +1,26 @@
 from dataclasses import dataclass
 from typing import Iterable, List, Tuple
 
+def parse_items_from_file(filename: str) -> Iterable[List[int]]:
+    with open(filename) as file:
+        for line in file:
+            yield [int(item) for item in line.rstrip().split(' ') if item]
+
 @dataclass
 class Matrix:
     lhs: List[int]
     rhs: List[int]
 
     def __init__(self, filename: str):
-        self.lhs = []
-        self.rhs = []
+        self.lhs, self.rhs = map(list, zip(*parse_items_from_file(filename)))
         
-        with open(filename) as file:
-            for line in file:
-                self.insert([item for item in line.rstrip().split(' ') if item])
-
-    def insert(self, items: List[str]) -> None:
-        if len(items) > 2:
-            raise ValueError(f"{len(items)=} is greater than 2")
-        
-        if not items:
-            return
-        
-        self.lhs.append(int(items[0]))
-        self.rhs.append(int(items[1]))
-
-    def sort(self) -> None:
-        self.lhs.sort()
-        self.rhs.sort()
     
     def iter_mins(self) -> Iterable[Tuple[int, int]]:
-        self.sort()
+        self.__sort()
 
         for item in zip(self.lhs, self.rhs):
             yield item
+
+    def __sort(self) -> None:
+        self.lhs.sort()
+        self.rhs.sort()
