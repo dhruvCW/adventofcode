6efc680f83b1e1399521ce873796a53cb903a54e
Dhruv Paranjape <d.paranjape@catawiki.nl>
2024-12-01 16:08:23 +0100

    Replace reduce with sum

--- 2024/src/day_01.py
+++ 2024/src/day_01.py
@@ -1,14 +1,13 @@
 from collections import Counter
-from functools import reduce
 from matrix import Matrix
 
 def total_distance(input: str) -> int:
     matrix = Matrix(input)
 
-    return reduce(lambda distance, items: distance + abs(items[0] - items[1]), matrix.iter_mins(), 0)
+    return sum(abs(lhs - rhs) for lhs, rhs in matrix.iter_mins())
 
 def similarity_score(input: str) -> int:
     matrix = Matrix(input)
     rhs_counter = Counter(matrix.rhs)
 
-    return reduce(lambda score, item: score + (item * rhs_counter[item]), matrix.lhs, 0)
+    return sum(item * rhs_counter[item] for item in matrix.lhs)
--- 2024/tests/test_day_01.py
+++ 2024/tests/test_day_01.py
@@ -1,10 +1,11 @@
 import pytest
 from day_01 import total_distance, similarity_score
 
-@pytest.mark.parametrize("data_file, distance", [('data/day_01_sample.txt', 11), ('data/day_01.txt', 1530215)])
-def test_total_distance(data_file, distance):
-    assert total_distance(data_file) == distance
+class TestDay01:
+    @pytest.mark.parametrize("data_file, distance", [('data/day_01_sample.txt', 11), ('data/day_01.txt', 1530215)])
+    def test_total_distance(self, data_file, distance):
+        assert total_distance(data_file) == distance
 
-@pytest.mark.parametrize("data_file, score", [('data/day_01_sample.txt', 31), ('data/day_01.txt', 26800609)])
-def test_similarity_score(data_file, score):
-    assert similarity_score(data_file) == score
+    @pytest.mark.parametrize("data_file, score", [('data/day_01_sample.txt', 31), ('data/day_01.txt', 26800609)])
+    def test_similarity_score(self, data_file, score):
+        assert similarity_score(data_file) == score
