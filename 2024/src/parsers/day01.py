5d81ca41a1b312eb803b5047e9bbdcc8a97d2b94
Dhruv Paranjape <d.paranjape@catawiki.nl>
2024-12-02 13:23:39 +0100

    simplify day01 parser.

--- 2024/src/parsers/day01.py
+++ 2024/src/parsers/day01.py
@@ -6,8 +6,8 @@ def parse_from_file(filename: str) -> Tuple[List[int], List[int]]:
     rhs = []
     with open(filename) as file:
         for line in file:
-            parsed_items = [int(item) for item in line.rstrip().split(' ') if item]
-            lhs.append(parsed_items[0])
-            rhs.append(parsed_items[1])
+            first, second = [int(item) for item in line.split()]
+            lhs.append(first)
+            rhs.append(second)
 
     return (lhs, rhs)
